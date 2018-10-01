2a07d79f205c0e7b46d31baef5b2c01d72a131ab
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-01 17:49:28 -0400

    feat: cover banner

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Cover from './Cover'
 import Website from './Website'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Website />
+      	<Cover />
+      	<Website />
       </div>
     );
   }
---
+++ src/Cover.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Cover.css';
+
+const Cover = (props) => {
+	return (
+		<section class="intro">
+			<div class="inner">
+				<div class="content">
+					<h1>Erika Miguel</h1>
+					<a class="btn" href="#">Welcome</a>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default Cover
