9c75436293240986d6548b098ca50af7f3e4dc3f
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-02 17:25:11 -0400

    feat: pulling blog posts from personal API

--- src/actions/blogs.js
+++ src/actions/blogs.js
@@ -7,42 +7,15 @@ function generateId () {
   return Math.random().toString(36).substring(2);
 }
 
-const blogs = [
-  {
-    id: generateId(),
-    title: "Hello, Welcome to My Blog!",
-    date: "October 2, 2018",
-    author: "Erika Miguel",
-    content: "This is my first ever post! I built this site from scratch, and I'm very excited about it."
-  },
-  {
-    id: generateId(),
-    title: "This is my second post!",
-    date: "October 2, 2018",
-    author: "Erika Miguel",
-    content: "For testing"
-  },
-  {
-    id: generateId(),
-    title: "This is my third post!",
-    date: "October 2, 2018",
-    author: "Erika Miguel",
-    content: "For testing"
-  }
-]
-
 // This is a thunk, whereby a function is dispatched, instead of an action
 // The action will be delayed until the "GET request" has been made
 export function handleInitialBlogData () {
   return (dispatch) => {
-    return new Promise((res, rej) => {
-    	setTimeout(() => {
-        res(blogs)
-    	}, 3000)
-    }).then((blogs) => {
-      dispatch(receiveData(blogs)) // send action to the store.
-      							               // store will then return a new state based on the reducer
-    })
+    fetch("https://api.erikamiguel.com/blog")   // This is my personal API. Check it out!
+      .then(data => data.json())
+      .catch(err => alert("There was an error!"))
+      .then(data => dispatch(receiveData(data.posts))) // send action to the store.
+                                                        // store will then return a new state based on the reducer
   }
 }
 
