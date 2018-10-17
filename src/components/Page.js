0a1a4aa5ce4218bc06ac6ac4cec15314628d8896
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-17 17:36:08 -0400

    minor: highlighting menu section clicked

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import './css/Page.css';
 import { connect } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
 
 class Page extends Component {
 	render () {
+		const { location } = this.props
 		return (
 			<div className='page-container'>
 				<section className="intro">
 					<div className="inner">
 						<div className="content">
-							<h1>Erika Miguel</h1>
+							<NavLink name="title" to="/"><h1>Erika Miguel</h1></NavLink>
 							<div className="icons">
-								<NavLink name="aboutButton" className="btn" to="/">About</NavLink>
-								<NavLink name="resumeButton" className="btn" to="/resume">Resume</NavLink>
-								<NavLink name="blogButton" className="btn" to="/blog">Blog</NavLink>
+								<NavLink name="aboutButton" className={`btn ${(!location.pathname.includes('resume') && !location.pathname.includes('blog')) ? 'selected' : ''}`} to="/">About</NavLink>
+								<NavLink name="resumeButton" className={`btn ${location.pathname.includes('resume') ? 'selected' : ''}`} to="/resume">Resume</NavLink>
+								<NavLink name="blogButton" className={`btn ${location.pathname.includes('blog') ? 'selected' : ''}`} to="/blog">Blog</NavLink>
 							</div>
 						</div>
 					</div>
@@ -24,4 +25,4 @@ class Page extends Component {
 	}
 }
 
-export default Page
+export default withRouter(Page)
