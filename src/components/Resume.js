dac302baeca069dd8ee5e9549ada30f9349e0e7c
Erika Miguel <ms.erikamiguel@gmail.com>
2018-10-16 21:52:41 -0400

    adding education and experience to resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -73,11 +73,51 @@ const Resume = (props) => {
 								</li>
 							</ul>
 						</div>
-
+						<div>
+							<h3>DEVOPS ENGINEER</h3>
+							<h3><span className='experience-co'>Perka</span>, New York, NY, April 2016 - March 2017</h3>
+							<ul>
+								<li>
+									<p className='experience-text'>Sole DevOps engineer on Platform team at the time. Served
+									on 24/7 call for production outages. Managed AWS infrastructure, SSL certs, server configuration,
+									and user on boarding / termination. Handled production deployment, PostgreSQL database migration,
+									and logging system</p>
+								</li>
+							</ul>
+						</div>
+						<div>
+							<h3>ANALYST SOFTWARE DEVELOPER</h3>
+							<h3><span className='experience-co'>Goldman Sachs</span>, New York, NY, July 2015 - April 2016</h3>
+							<ul>
+								<li>
+									<p className='experience-text'>DevOps engineering role in Global Investment
+									Research; automated deployments and migrated apps to new config service. Software
+									developer role in Prime Services division; developed access controlled file processing
+									feature for legacy app</p>
+								</li>
+							</ul>
+						</div>
 					</section>
 				</div>
 				<div className='education entry'>
 					<h2 className='resume-header'>Education</h2>
+					<section className='education-section'>
+						<div>
+							<h3>SMITH COLLEGE</h3>
+							<h4>Bachelor of Arts, Computer Science</h4>
+							<h4>September 2011 - May 2015</h4>
+						</div>
+						<div>
+							<h3>UDACITY</h3>
+							<h4>Nanodegree, Front End Web Developer</h4>
+							<h4>September 2018 - Present</h4>
+						</div>
+						<div>
+							<h3>UDACITY</h3>
+							<h4>Nanodegree, React Developer</h4>
+							<h4>September 2018 - Present</h4>
+						</div>
+					</section>
 				</div>
 				<div className='projects entry'>
 					<h2 className='resume-header'>Projects</h2>
