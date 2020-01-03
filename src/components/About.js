b56effab390ea394216c37d233bcbc08d7d0a1e4
Erika Miguel-Yeo <erika@bocoup.com>
2020-01-03 15:42:25 -0500

    updating about

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = (props) => {
 	    	<div className='about__text'>
 	    		<h2>Hi! I'm Erika.</h2>
 	    		<p className='about__description'>
-				Erika is a first generation, GNC, tech professional who goes by she or they pronouns.
-				She thrives in small teams and diverse, open-minded environments. Erika is currently
+				Erika is a first generation, GNC, tech professional who goes by they/them pronouns.
+				They thrives in small teams and diverse, open-minded environments. Erika is currently
 				an engineer at Bocoup and Smith College graduate in Computer Science. They have a breadth
 				of technical experience ranging from Front End and npm package development in Javascript,
 				cloud development in AWS and GCP, and CI/CD/Release management specifically with hardware
@@ -16,10 +16,9 @@ const About = (props) => {
 				</p>
 				<p className='about__description'>
 				Erika believes in uplifting the voices of marginalized groups in tech, particularly women,
-				trans / GNC folks, and people of color. She believes in accessibility, work life balance,
-				and listening to one's needs. She lives in Quincy, Massachusetts with her lovely wife (and
-				best friend), Megan. They love to garden, cook, exercise, travel, and learn new things.
-				Erika is currently exploring machine learning and data visualization off-hours.
+				trans / GNC folks, and people of color. They believe in accessibility, work life balance,
+				and listening to one's needs. They live in Quincy, Massachusetts and is passionate about learning,
+				fitness, gardening, and music.
 	    		</p>
 	    	</div>
 	    </div>
