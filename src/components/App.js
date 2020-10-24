38be08d73acef091bba7ec0d44d968f170558820
Erika Miguel <erika@erikamiguel.com>
2020-10-24 16:23:06 -0400

    Delete superfluous code

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './css/App.css';
-import Page from './Page'
-import { connect } from 'react-redux'
-import Blogs from './Blogs'
-import BlogPost from './BlogPost'
-import { handleInitialBlogData } from '../actions/blogs'
-import About from './About'
-import Resume from './Resume'
-import Projects from './Projects'
-import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 
 class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(handleInitialBlogData())
-  }
-
   render() {
     return (
-      <Router>
-        <div className="App">
+      <div className="App">
 
-          <Page />
-          <Route exact path="/" component={About} />
-          <Route path="/resume" component={Resume} />
-          <Route exact path="/blog" component={Blogs}/>
-          <Route path="/blog/:id" component={BlogPost} />
-          <Route path="/projects" component={Projects} />
 
-        <footer className="app__footer">
-          <nav className='social-media'>
-            <a href="https://github.com/evmiguel" target='_blank' rel="noopener noreferrer"><FaGithub className='icon'/></a>
-            <a href="https://www.linkedin.com/in/evmiguel" target="_blank" rel="noopener noreferrer"><FaLinkedin className='icon'/></a>
-            <a href="https://www.instagram.com/evmigz" target='_blank' rel="noopener noreferrer"><FaInstagram className='icon'/></a>
-          </nav>
-          <p>This page is powered by GCP Firebase hosting, React, Redux, and DynamoDB. The entirety of this site has been created by Erika Miguel. <a href='https://github.com/evmiguel/personal_site' target='_blank' rel="noopener noreferrer">Source code</a></p>
-        </footer>
-
-        </div>
-      </Router>
+      <footer className="app__footer">
+        <p>This page is powered by GCP Firebase hosting. The entirety of this site has been created by Erika Miguel. <a href='https://github.com/evmiguel/personal_site' target='_blank' rel="noopener noreferrer">Source code</a></p>
+      </footer>
+      </div>
     );
   }
 }
 
-export default connect()(App)
+export default App;
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import loading from './loading'
-import blogs from './blogs'
-
-export default combineReducers({
-  loading,
-  blogs,
-})
